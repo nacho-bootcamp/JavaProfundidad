7d2e615c6d714f0d0316624c8aad06bcbf758898
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-10-03 23:49:51 -0300

    creacion de archivos

---
+++ Seccion3/HolaMundo.java
@@ -0,0 +1,7 @@
+package Seccion3;
+
+class HolaMundo {
+  public static void main(String[] args) {
+    System.out.println("Hola Mundo");
+  }
+}
---
+++ Seccion3/developer.java
@@ -0,0 +1,12 @@
+package Seccion3;
+
+// esta se puede hacer pero cuando una clase no tiene un modificador de acceso
+//el archivo se puede llamar como quieramos
+class uno {
+}
+
+class dos {
+}
+
+class tres {
+}
