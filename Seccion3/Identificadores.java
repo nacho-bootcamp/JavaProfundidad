b00b94154d5ff7e63b0d368bee1ca7d132b342f2
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-10-04 00:38:50 -0300

    identificadores y tipos de datos

---
+++ Seccion3/Identificadores.java
@@ -0,0 +1,16 @@
+package Seccion3;
+
+public class Identificadores {
+
+  public static void main(String args[]) {
+
+    // LOS IDENTIFICADORES NO PUEDEN EMPEZAR CON UN NUMERO
+    // double 1sueldo=10000.5;
+    // NO PODEMOS UTILIZAR PALABRAS RESERVADAS COMO DOUBLE, FINAL,ETC.
+
+    double sueldo = 10000.5;
+    double _ = 10000.115;
+    double $ = 10000000.5;
+
+  }
+}
---
+++ Seccion3/TiposDeDatos.java
@@ -0,0 +1,18 @@
+package Seccion3;
+
+public class TiposDeDatos {
+  public static void main(String args[]) {
+    // tipo de datos / identificador = literal
+    // con final definimos una constante y el literal no debe cambiar
+    final int edad = 18;
+    System.out.println("Mi edad es :" + edad);
+
+    byte tamanio = 50;
+    char caracter = 'C';
+    short superficieHabitable = 1023;
+    long distancia = 50505051L;
+    boolean isActive = false;
+    float sueldo = 100000.015F;
+    double superficie = 1000000000000.25555;
+  }
+}
