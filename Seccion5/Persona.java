508cc1ce252cff0e3decb276e139cd1688525496
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-10-05 13:17:34 -0300

    Uso de parametro

--- Seccion5/Persona.java
+++ Seccion5/Persona.java
@@ -8,10 +8,28 @@ public class Persona {
   char genero;
 
   // Metodos
-  void Informacio() {
+  void Informacion() {
     System.out.println("Nombre: " + nombre);
     System.out.println("Edad: " + edad);
     System.out.println("Genero: " + genero);
+  }
 
+  void JugarVideosJuegos(Persona p) {
+    System.out.println(nombre + " Esta jugando con " + p.nombre);
+  }
+
+  public static void main(String[] args) {
+    Persona p = new Persona();
+    p.nombre = "Eri";
+    p.edad = 25;
+    p.genero = 'F';
+    p.Informacion();
+
+    Persona p1 = new Persona();
+    p1.nombre = "Nacho";
+    p1.edad = 22;
+    p1.genero = 'M';
+    p1.Informacion();
+    p1.JugarVideosJuegos(p);
   }
 }
