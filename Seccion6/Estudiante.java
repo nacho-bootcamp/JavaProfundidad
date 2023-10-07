6a478b44d86d14983fe739cd587b4a8a30719c3a
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-10-07 18:29:28 -0300

    Objetos y Clases finales

--- Seccion6/Estudiante.java
+++ Seccion6/Estudiante.java
@@ -1,11 +1,24 @@
 package Seccion6;
 
+// si ponemos final a una clase lo que hace es que otras clases no puedan
+//heredar de esta clase
 public class Estudiante extends Persona {
   int numeroDeCuenta;
 
   Estudiante(int numeroDeCuenta, String nombre, String fechaDeNacimiento) {
     super(nombre, fechaDeNacimiento);
     this.numeroDeCuenta = numeroDeCuenta;
+  }
+  // Esto se llama Sobre Escritura de Metodo
+  // Para Sobre escribir un metodo se necesita la notacion
+  // @Override esto me permite que renunciemos al metodo que posee la clase padre
+
+  // con this no referimos a nosotros mismo con super no referimos a la clase
+  // padre
+  @Override
+  void dormir() {
+    super.dormir();
+    System.out.println("Soy " + nombre + " y yo no duermo");
   }
 
   void aprobar() {
@@ -20,3 +33,8 @@ public class Estudiante extends Persona {
     return nota;
   }
 }
+
+// final en clases no puedo heredar de la clase que estoy marcando como final
+// final en metodos esos metodos no se pueden sobreescribir
+// final en tipos de datos primitivos se vuelven constantes
+// final en objetos no puedo modificar la referencia de ese objeto
