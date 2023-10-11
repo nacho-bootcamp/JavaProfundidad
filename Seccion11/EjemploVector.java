2bf9b57da32ba04ee9421afb61b9fb81ca1360ba
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-10-11 20:17:36 -0300

    Vectores

---
+++ Seccion11/EjemploVector.java
@@ -0,0 +1,14 @@
+package Seccion11;
+
+import java.util.Vector;
+
+public class EjemploVector {
+  public static void main(String[] args) {
+    // se utiliza si el acceso ess controlado
+    Vector<String> lista = new Vector<>();
+    lista.add("nacho");
+    lista.add("Consiti");
+    lista.add("Java");
+    System.out.println(lista);
+  }
+}
