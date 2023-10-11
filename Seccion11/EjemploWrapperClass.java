2ef77351a034cc83ecb0a756df2bdd6ba73ec803
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-10-11 18:28:57 -0300

    Wrapper

---
+++ Seccion11/EjemploWrapperClass.java
@@ -0,0 +1,12 @@
+package Seccion11;
+
+public class EjemploWrapperClass {
+  public static void main(String[] args) {
+    // dato primitivos
+    int x = 10;
+    // lo combierte en un Objeto y tiene metodos
+    Integer x1 = 100;
+    System.out.println(x1.floatValue());
+    System.out.println(x);
+  }
+}
---
+++ Seccion11/MetodoDeConversion.java
@@ -0,0 +1,14 @@
+package Seccion11;
+
+public class MetodoDeConversion {
+  public static void main(String[] args) {
+    Integer x = 10;
+    double d = x.doubleValue();
+    float f = x.floatValue();
+    long l = x.longValue();
+
+    String valor = "10";
+    int val = Integer.parseInt(valor);
+    System.out.println(val);
+  }
+}
