2d241055bd1176caa9a17780d0b2cab630636612
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-10-11 18:36:19 -0300

    Autoboxing y Auto-Unboxing

---
+++ Seccion11/Conversion.java
@@ -0,0 +1,11 @@
+package Seccion11;
+
+public class Conversion {
+  public static void main(String[] args) {
+    // Autoboxing
+    Integer i = 10;
+    // Auto-Unboxing
+    int y = i;
+    System.out.println(y);
+  }
+}
