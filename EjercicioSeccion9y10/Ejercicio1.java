d8edc382fe12650ec438bd4729e4b786c6b3156c
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-10-10 21:38:57 -0300

    Ejercicio uno terminado

---
+++ EjercicioSeccion9y10/Ejercicio1.java
@@ -0,0 +1,29 @@
+package EjercicioSeccion9y10;
+
+public class Ejercicio1 {
+  public static void main(String[] args) {
+
+    boolean datoBoolean[] = new boolean[2];
+    datoBoolean[0] = false;
+    datoBoolean[1] = true;
+
+    byte datoByte[] = { 12, 32, 53 };
+
+    short datoShort[] = new short[] { 1213, 4343, 54 };
+
+    int datoInt[] = new int[2];
+    datoInt[0] = 7;
+    datoInt[1] = 97;
+
+    char datoChar[] = { 'N', 'C', 'E' };
+
+    float datoFloat[] = new float[] { 12.5f, 18.9f, 28.6f };
+
+    long datoLong[] = { 124214214, 35235235, 234523423 };
+
+    double datoDouble[] = new double[2];
+    datoDouble[0] = 31.32;
+    datoDouble[1] = 21.3;
+  }
+
+}
---
+++ EjercicioSeccion9y10/README.md
@@ -0,0 +1,9 @@
+## Seccion 9 y 10 Temas Generales
+
+1. Crear un arreglo de cada tipo de dato **boolean ,byte ,short ,int ,char ,float ,long y double.**
+
+2. Crear un arreglo irregular en forma de **triangulo.**
+
+3. Crear un arreglo de tipo Mascota y llenarlo con **Objetos de diferentes Subtipos.**
+
+4. Realizar **2 arreglos bidimensionales** de las misma dimensiones y sumarlos.
