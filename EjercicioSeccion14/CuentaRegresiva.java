b14aef8064af37ce76bb4fe45ee7dfb25d5381ba
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-10-14 17:23:23 -0300

    ejercicio 1 terminado

---
+++ EjercicioSeccion14/CuentaRegresiva.java
@@ -0,0 +1,52 @@
+package EjercicioSeccion14;
+
+// ejercicio 1
+import java.util.Random;
+
+class Counter extends Thread {
+  private int valor;
+
+  Counter(int valor, String name) {
+    super(name);
+    this.valor = valor;
+  }
+
+  @Override
+  public void run() {
+    System.out.printf("Hilo %s estado: %s%n", getName(), getState());
+    for (int i = valor; i >= 0; i--) {
+      System.out.printf("Hilo %s indice %d%n", getName(), i);
+    }
+    try {
+      Thread.sleep(new Random().nextInt(500));
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+}
+
+public class CuentaRegresiva {
+  public static void main(String[] args) {
+    Counter[] hilo = new Counter[10];
+
+    for (int i = 0; i < 10; i++) {
+      hilo[i] = new Counter(new Random().nextInt(15), "contador" + i);
+      hilo[i].start();
+    }
+    try {
+      Counter hiloGanador = null;
+      for (Counter hilo1 : hilo) {
+        System.out.printf("Hilo %s ha terminado.%n", hilo1.getName());
+        if (hiloGanador == null || hilo1.getState() == Thread.State.TERMINATED) {
+          hiloGanador = hilo1;
+          hiloGanador.join();
+        }
+      }
+      System.out.printf("El hilo ganador es: %s%n", hiloGanador.getName());
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+  }
+}
---
+++ EjercicioSeccion14/README.md
@@ -0,0 +1,5 @@
+## Programacion con hilos
+
+1. Crea un programa donde un hilo haga una cuenta regresiva dando un numero, el tiempo que debe esperar el hilo debe ser aleatorio. **Inicia 10 hilos y revisa que hilo termino primero** asegurate de colocarle un nombre a cada uno de ellos.
+
+2. Modificar el programa anterior para que todos los hilos esperen el mismo tiempo **asignale diferentes prioridades a cada uno de ellos** y lanza 30 hilos,revisa si el cambio prioridad genero algun cambio.
