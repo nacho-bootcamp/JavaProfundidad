fb63793ec60746532f3e166fe90b3aa89bb155bb
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-10-14 11:15:15 -0300

    hilo productor

--- Seccion14/producerconsumers/DevsProducer.java
+++ Seccion14/producerconsumers/DevsProducer.java
@@ -1,5 +1,24 @@
 package Seccion14.producerconsumers;
 
-public class DevsProducer {
+import java.util.Random;
 
+public class DevsProducer extends Thread {
+  private DevsResource resource;
+
+  public DevsProducer(DevsResource resource, String name) {
+    super(name);
+    this.resource = resource;
+  }
+
+  @Override
+  public void run() {
+    while (true) {
+      try {
+        resource.produce(new Random().nextInt(1000));
+        Thread.sleep(new Random().nextInt(150));
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
 }
